a3a9d0c976f319c7a0bb2e60c6e860043c0a736a
Shubham Shukla <25775974+ShubhamShukla13@users.noreply.github.com>
2026-08-09 22:27:09 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int start=0;
+        int end=s.length()-1;
+        while(start<end){
+            char st=s.charAt(start);
+            char en=s.charAt(end);
+            System.out.println("Start :: "+st+ "||| END : "+ en);
+            if(!isAlphaNumeric(st)){
+                start++;
+                continue;
+            }
+            if(!isAlphaNumeric(en)){
+                end--;
+                continue;
+            }
+            if(Character.toLowerCase(st)==Character.toLowerCase(en)){
+                start++;
+                end--;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean isAlphaNumeric(char c){
+        int ch=c+0;
+        if((ch>=('a'-0) && ch<=('z'-0))||(ch>=('A'-0) && ch<=('Z'-0)) || (ch>=0 && ch<=9)) {
+            return true;
+        }else return false;
+    }
+}
